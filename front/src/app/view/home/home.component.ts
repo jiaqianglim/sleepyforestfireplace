6c705cbf93bf288525996f3eb8051278278c90bd
ljq <jqlimjiaqiang@gmail.com>
2023-04-18 12:27:50 +0800

    first commit

--- front/src/app/view/home/home.component.ts
+++ front/src/app/view/home/home.component.ts
@@ -1,7 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Subject, takeUntil } from 'rxjs';
 import { Topic } from 'src/app/model/topic';
 import { LiveTopicService } from 'src/app/service/livetopic.service';
+
+interface categoryoption {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -11,8 +18,21 @@ import { LiveTopicService } from 'src/app/service/livetopic.service';
 export class HomeComponent implements OnInit, OnDestroy {
   livetopics!: Topic[];
   destroy$ = new Subject<void>();
+  filterform!: FormGroup;
 
-  constructor(private livetopicservice: LiveTopicService) {}
+  categories: categoryoption[] = [
+    { value: 'featured', viewValue: 'Featured' },
+    { value: 'release_local', viewValue: 'Live Around You' },
+    { value: 'release_news', viewValue: 'Live: World News' },
+    { value: 'release_sports', viewValue: 'Live: Sports' },
+    { value: 'release_tvshows', viewValue: 'Live: TV Episodes' },
+    { value: 'release_politics', viewValue: 'Live: Government & Politics' },
+  ];
+  constructor(
+    private livetopicservice: LiveTopicService,
+    private http: HttpClient,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.livetopicservice
@@ -21,10 +41,15 @@ export class HomeComponent implements OnInit, OnDestroy {
       .subscribe((data) => {
         this.livetopics = data;
       });
+    this.filterform = this.fb.group({
+      queryString: this.fb.control<string>(''),
+    });
   }
 
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
   }
+
+  onProcessFilterForm() {}
 }
--- front/src/app/view/topic/topic.component.ts
+++ front/src/app/view/topic/topic.component.ts
@@ -1,11 +1,18 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-topic',
   templateUrl: './topic.component.html',
   styleUrls: ['./topic.component.css'],
 })
-export class TopicComponent {
-  constructor(private http: HttpClient) {}
+export class TopicComponent implements OnInit, OnDestroy {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+
 }
