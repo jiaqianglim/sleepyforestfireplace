c1e16d83981f609c6e338e59f0a8b47facf7e6a3
ljq <jqlimjiaqiang@gmail.com>
2023-04-19 12:32:58 +0800

    first commit

--- front/src/app/module/app-routing.module.ts
+++ front/src/app/module/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../view/home/home.component';
-import { LoginComponent } from '../security/login/login.component';
+import { AuthFormComponent } from '../security/auth-form/auth-form.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'userauth', component: LoginComponent },
+  { path: 'userauth', component: AuthFormComponent },
 ];
 
 @NgModule({
--- front/src/app/view/topicform/topicform.component.ts
+++ front/src/app/view/topicform/topicform.component.ts
@@ -1,15 +1,32 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-topicform',
   templateUrl: './topicform.component.html',
   styleUrls: ['./topicform.component.css'],
 })
-export class TopicformComponent {
-  
+export class TopicformComponent implements OnInit {
+  topicForm!: FormGroup;
+  topicTab!: FormArray;
   constructor(private fb: FormBuilder, private http: HttpClient) {}
+  ngOnInit(): void {
+    this.topicForm = this.fb.group({
+      name: this.fb.control<string>('', Validators.required),
+      description: this.fb.control<string>('', [
+        Validators.maxLength(1000),
+        Validators.required,
+      ]),
+      date: this.fb.control<string>('', [
+        Validators.required,
+        Validators.pattern(
+          /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/
+        ),
+      ]),
+    });
+  }
 
+  addcustomtab() {}
   processform() {}
 }
