05f523e84f112bd32fcef24827606b5919b4fe00
jiaqianglim <jqlimjiaqiang@gmail.com>
2023-04-20 21:42:08 +0800

    first commit

---
+++ webmvc/src/main/java/com/togetherness/webmvc/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.togetherness.webmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MongoConfig {
+
+}
